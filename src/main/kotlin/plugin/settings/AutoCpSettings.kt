b94f15278fb783164007c381a1820bdaa7b9c2df
Pushpavel <jpushpavel@gmail.com>
2021-06-07 16:45:25 +0530

    settings serialization bug fix

--- src/main/kotlin/plugin/settings/AutoCpSettings.kt
+++ src/main/kotlin/plugin/settings/AutoCpSettings.kt
@@ -8,28 +8,25 @@ import com.intellij.util.xmlb.XmlSerializerUtil
     storages = [Storage("autoCpPluginSettings.xml")]
 )
 @Service
-class AutoCpSettings : PersistentStateComponent<AutoCpSettings> {
-    var preferredLanguage: String? = "cpp"
-    var solutionLanguages: MutableList<SolutionLanguage> = getDefaultSolutionLanguages()
-    var selectedIndex: Int? = 0
+data class AutoCpSettings(
+    var preferredLanguage: String? = "cpp",
+    var solutionLanguages: MutableList<SolutionLanguage> = getDefaultSolutionLanguages(),
+    var selectedIndex: Int? = 0,
+) : PersistentStateComponent<AutoCpSettings> {
 
     override fun getState() = this
 
     override fun loadState(state: AutoCpSettings) = XmlSerializerUtil.copyBean(state, this)
 
+
     companion object {
         val instance = service<AutoCpSettings>()
-
         val DUPLICATE_NAME_REGEX = Regex("^(.*)_([0-9]+)\$")
-        val INPUT_PATH_KEY = "@input@"
-        val OUTPUT_PATH_KEY = "@output@"
+        const val INPUT_PATH_KEY = "@input@"
+        const val OUTPUT_PATH_KEY = "@output@"
 
         fun getSolutionLanguageTemplate(): SolutionLanguage {
-            return SolutionLanguage(
-                "C++",
-                "cpp",
-                "g++ $INPUT_PATH_KEY -o $OUTPUT_PATH_KEY"
-            )
+            return SolutionLanguage("C++", "cpp", "g++ $INPUT_PATH_KEY -o $OUTPUT_PATH_KEY")
         }
 
         fun getDefaultSolutionLanguages(): MutableList<SolutionLanguage> {
--- src/main/kotlin/plugin/settings/SolutionLanguage.kt
+++ src/main/kotlin/plugin/settings/SolutionLanguage.kt
@@ -1,12 +1,10 @@
 package plugin.settings
 
 data class SolutionLanguage(
-    var name: String,
-    var extension: String,
-    var buildCommand: String,
-
-
-    ) {
+    var name: String = "",
+    var extension: String = "",
+    var buildCommand: String = "",
+) {
     override fun toString(): String {
         return name
     }
