a30b6061b97566ebf46ddc73ccebcd4709f0dc52
Pushpavel <jpushpavel@gmail.com>
2021-06-11 22:49:55 +0530

    bug fix: org.slf4j errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,10 +31,14 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
     implementation("org.xerial:sqlite-jdbc:3.34.0")
 
-
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
     testImplementation("io.mockk:mockk:1.10.6")
+
+
+    configurations.forEach {
+        it.exclude("org.slf4j", "slf4j-api")
+    }
 }
 
 // Configure gradle-intellij-plugin plugin.
