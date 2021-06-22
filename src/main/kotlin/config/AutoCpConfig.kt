9f4e57e380a641d1e0d6ce4245796130a31400db
Pushpavel <jpushpavel@gmail.com>
2021-06-22 20:19:44 +0530

    💡: documented classes in config package

--- src/main/kotlin/config/AutoCpConfig.kt
+++ src/main/kotlin/config/AutoCpConfig.kt
@@ -8,20 +8,31 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.JDOMExternalizerUtil
 import org.jdom.Element
 
+/**
+ * Implementation Class for a Custom Run Configuration that can also be created from context (by right-clicking and run)
+ */
 class AutoCpConfig(project: Project, factory: ConfigurationFactory, name: String) :
     LocatableConfigurationBase<RunProfileState>(project, factory, name) {
-    companion object {
-        private const val SOLUTION_FILE_PATH = "solutionFilePath"
-        private const val SOLUTION_LANG_ID = "solutionLang"
-    }
 
     var solutionFilePath: String = ""
     var solutionLangId: Long = -1
 
+
+    /**
+     * Returns [RunProfileState] that defines the execution of this Run Configuration
+     */
     override fun getState(executor: Executor, environment: ExecutionEnvironment) = AutoCpRunState(this)
 
+
+    /**
+     * Returns UI Editor for this Run Configuration
+     */
     override fun getConfigurationEditor() = ConfigEditor(project)
 
+
+    /**
+     * Suggests Name for Run configurations created from Context (by right-clicking and run)
+     */
     override fun suggestedName(): String? {
         if (solutionFilePath.isEmpty())
             return null
@@ -29,7 +40,12 @@ class AutoCpConfig(project: Project, factory: ConfigurationFactory, name: String
     }
 
 
-    // SERIALIZATION
+    /** SERIALIZATION */
+    companion object {
+        // SERIALIZATION KEYS
+        private const val SOLUTION_FILE_PATH = "solutionFilePath"
+        private const val SOLUTION_LANG_ID = "solutionLang"
+    }
 
     override fun writeExternal(element: Element) {
         JDOMExternalizerUtil.writeField(element, SOLUTION_FILE_PATH, solutionFilePath)
