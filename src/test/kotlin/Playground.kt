401b39f8b55750b91d88fe439ea095dff3968a9b
Pushpavel <jpushpavel@gmail.com>
2021-06-23 08:45:06 +0530

    🚚:Reorganized Test packages

---
+++ src/test/kotlin/Playground.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test
+
+class Playground {
+
+
+    @Test
+    fun play() {
+    }
+
+}
--- src/test/kotlin/bugs/DatabaseFileIsNotClosedBug.kt
+++ src/test/kotlin/bugs/DatabaseFileIsNotClosedBug.kt
@@ -1,10 +1,10 @@
-package tests
+package bugs
 
+import com.github.pushpavel.autocp.database.AutoCpDatabaseTransactor
+import com.github.pushpavel.autocp.database.Problem
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
 import database.adapters.DataColumnAdapter
 import database.adapters.TestcaseColumnAdapter
-import com.github.pushpavel.autocp.database.AutoCpDatabaseTransactor
-import com.github.pushpavel.autocp.database.Problem
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
@@ -13,21 +13,16 @@ import java.nio.file.Path
 import java.nio.file.Paths
 import kotlin.io.path.pathString
 
-class Playground {
+class DatabaseFileIsNotClosedBug {
 
     @Test
-    @Disabled("there is a bug in 3rd party library")
-    fun bug(@TempDir dir: Path) {
+    @Disabled("this bug is in a 3rd party library")
+    fun solved(@TempDir dir: Path) {
         val dbFilePath = Paths.get(dir.pathString, "data.db")
         val driver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY + dbFilePath.pathString)
-        AutoCpDatabaseTransactor(driver, Problem.Adapter(TestcaseColumnAdapter(),DataColumnAdapter()))
+        AutoCpDatabaseTransactor(driver, Problem.Adapter(TestcaseColumnAdapter(), DataColumnAdapter()))
         AutoCpDatabaseTransactor.Schema.create(driver)
         driver.getConnection().close() // this does not close the file (bug)
         Files.delete(dbFilePath)
     }
-
-//    @Test
-    fun play() {
-    }
-
 }
