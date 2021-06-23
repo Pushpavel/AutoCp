14406bfe328f823c6323f3dde2572121f050b5f1
Pushpavel <jpushpavel@gmail.com>
2021-06-23 11:03:06 +0530

    🎨:improved functional error handling sealed classes hierarchy

--- src/main/kotlin/common/errors/Result.kt
+++ src/main/kotlin/common/errors/Result.kt
@@ -3,8 +3,11 @@ package common.errors
 /**
  * inspired from https://kotlin.christmas/2019/17
  * though com.jetbrains.rd.util.Result has similar implementation, it cannot be extended
+ *
+ * General Result Type
  */
-sealed class Result<T> {
-    class Ok<T>(val value: T) : Result<T>()
-    class Err(val error: Throwable) : Result<Nothing>()
-}
+sealed interface Result<out T>
+
+class Ok<out T> : Result<T>, TpResult<T>
+
+sealed class Err(message: String) : Throwable(message), Result<Nothing>
