192579a0b91ecfc47eba1ed13de891d06af566ca
Pushpavel <jpushpavel@gmail.com>
2021-07-12 23:45:13 +0530

    ➕: added rxkotlin dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.5.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.10")
+    implementation("io.reactivex.rxjava3:rxkotlin:3.0.1")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
