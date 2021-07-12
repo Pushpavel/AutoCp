63f04e156dcdf9a8004f791fe2260049dc1b999b
Pushpavel <jpushpavel@gmail.com>
2021-07-12 19:08:54 +0530

    🚧: added AutoCpLangSettingsConfigurable.kt

---
+++ src/main/kotlin/settings/langSettings/AutoCpLangSettingsConfigurable.kt
@@ -0,0 +1,22 @@
+package settings.langSettings
+
+import com.intellij.openapi.options.Configurable
+import javax.swing.JComponent
+
+class AutoCpLangSettingsConfigurable : Configurable {
+    override fun createComponent(): JComponent? {
+        TODO("Not yet implemented")
+    }
+
+    override fun isModified(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun apply() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getDisplayName(): String {
+        TODO("Not yet implemented")
+    }
+}
