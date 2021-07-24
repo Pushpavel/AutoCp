9a76dca8660e145a3a0e409ba4492112d08f6b02
pushpavel <jpushpavel@gmail.com>
2021-07-24 18:28:38 +0530

    🚚: moved comboBoxSelection.kt to swingBindings package

--- src/main/kotlin/ui/vvm/swingBinding/comboBox.kt
+++ src/main/kotlin/ui/vvm/swingBinding/comboBox.kt
@@ -9,7 +9,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import ui.vvm.swingModels.bindSelectionIndex
 import ui.vvm.swingModels.update
 
 fun <T> CoroutineScope.bind(
