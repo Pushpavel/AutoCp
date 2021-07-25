d389c21ac734f0e40b70aee085461a5aefb31265
pushpavel <jpushpavel@gmail.com>
2021-07-25 14:08:37 +0530

    🚧: selecting default language in generate solution dialog

--- src/main/kotlin/gather/ui/solutionsDialog/SolutionsDialogModel.kt
+++ src/main/kotlin/gather/ui/solutionsDialog/SolutionsDialogModel.kt
@@ -4,12 +4,16 @@ import com.github.pushpavel.autocp.database.Problem
 import com.intellij.openapi.project.Project
 import com.intellij.ui.CollectionComboBoxModel
 import com.intellij.ui.CollectionListModel
+import settings.generalSettings.AutoCpGeneralSettings
 import settings.langSettings.AutoCpLangSettings
 import javax.swing.Icon
 
 class SolutionsDialogModel(val project: Project, problems: List<Problem>) {
     val listModel = CollectionListModel(problems)
     val groupName = problems.firstOrNull()?.groupName
-    val langModel = CollectionComboBoxModel(AutoCpLangSettings.getLanguages())
+    val langModel = CollectionComboBoxModel(AutoCpLangSettings.getLanguages(), run {
+        val selectedLangId = AutoCpGeneralSettings.getPreferredLangId()
+        AutoCpLangSettings.getLanguages().firstOrNull { it.langId == selectedLangId }
+    })
     var langIcon: Icon? = null
 }
