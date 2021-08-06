561173ceda49e848d41ea5f9c1cff6b5d89a0624
pushpavel <jpushpavel@gmail.com>
2021-08-06 10:23:10 +0530

    🚧: added UniqueNameEnforcer for testcases

--- src/main/kotlin/tool/ui/TestcaseListPanel.kt
+++ src/main/kotlin/tool/ui/TestcaseListPanel.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import com.intellij.ui.CollectionListModel
+import common.helpers.UniqueNameEnforcer
 import database.models.SolutionFile
 import database.models.Testcase
 import ui.swing.editableList.EditableListView
@@ -12,6 +13,11 @@ import javax.swing.JComponent
 class TestcaseListPanel(project: Project, solutionFile: SolutionFile) : Disposable {
 
     private val testcaseListModel = CollectionListModel(solutionFile.testcases)
+    private val testcaseNameEnforcer = UniqueNameEnforcer(
+        Regex("^(.*) #([0-9]+)\$"),
+        { p, s -> "$p #$s" },
+        { testcaseListModel.items.map { it.name } }
+    )
 
     val component: JComponent
 
@@ -19,7 +25,7 @@ class TestcaseListPanel(project: Project, solutionFile: SolutionFile) : Disposab
         component = EditableListView(testcaseListModel, {
             TestcasePanel(testcaseListModel)
         }, {
-            Testcase("New Testcase", "input", "output")
+            Testcase(testcaseNameEnforcer.buildUniqueNameWithPrefix("Testcase"), "input", "output")
         }, "New Testcase")
         Disposer.register(this, component)
 
