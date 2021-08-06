52dbaa6de2b8eb6a2ecaeb7a73461834e85f7f81
pushpavel <jpushpavel@gmail.com>
2021-08-06 15:37:26 +0530

    ➖: removed sqldelight package

--- src/main/kotlin/database/models/ProblemExtension.kt
+++
@@ -1,8 +0,0 @@
-package database.models
-
-import com.github.pushpavel.autocp.database.Problem
-
-@Deprecated("use database.models.Problem")
-fun Problem.getTimeLimit(): Long {
-    return this.data_.get("timeLimit").asLong
-}
