ad2ef0a907194d996061a143a7a2400fa5c1e594
pushpavel <jpushpavel@gmail.com>
2021-08-06 11:13:51 +0530

    🚧: added saving AutoCp Tool to AutoCpDB.kt

--- src/main/kotlin/tool/ui/TestcaseListPanel.kt
+++ src/main/kotlin/tool/ui/TestcaseListPanel.kt
@@ -5,12 +5,15 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import com.intellij.ui.CollectionListModel
 import common.helpers.UniqueNameEnforcer
+import database.autoCp
 import database.models.SolutionFile
 import database.models.Testcase
 import ui.swing.editableList.EditableListView
 import javax.swing.JComponent
+import javax.swing.event.ListDataEvent
+import javax.swing.event.ListDataListener
 
-class TestcaseListPanel(project: Project, solutionFile: SolutionFile) : Disposable {
+class TestcaseListPanel(project: Project, private val solutionFile: SolutionFile) : Disposable, ListDataListener {
 
     private val testcaseListModel = CollectionListModel(solutionFile.testcases)
     private val testcaseNameEnforcer = UniqueNameEnforcer(
@@ -19,19 +22,57 @@ class TestcaseListPanel(project: Project, solutionFile: SolutionFile) : Disposab
         { testcaseListModel.items.map { it.name } }
     )
 
+    val db = project.autoCp()
     val component: JComponent
 
     init {
-        component = EditableListView(testcaseListModel, {
-            TestcasePanel(testcaseListModel)
-        }, {
-            Testcase(testcaseNameEnforcer.buildUniqueNameWithPrefix("Testcase"), "input", "output")
-        }, "New Testcase")
+        component = EditableListView(
+            testcaseListModel,
+            { TestcasePanel(testcaseListModel) },
+            {
+                val name = testcaseNameEnforcer.buildUniqueNameWithPrefix("Testcase")
+                Testcase(name, "input", "output")
+            },
+            "New Testcase"
+        )
         Disposer.register(this, component)
 
+        testcaseListModel.addListDataListener(this)
     }
 
-    override fun dispose() {}
+
+    override fun intervalAdded(event: ListDataEvent) {
+        updateTestcases { testcases ->
+            for (i in event.index0..event.index1)
+                testcases.add(i, testcaseListModel.items[i])
+        }
+    }
+
+    override fun intervalRemoved(event: ListDataEvent) {
+        updateTestcases { testcases ->
+            testcases.subList(event.index0, event.index1 + 1).clear()
+        }
+    }
+
+    override fun contentsChanged(event: ListDataEvent) {
+        updateTestcases { testcases ->
+            for (i in event.index0..event.index1)
+                testcases[i] = testcaseListModel.items[i]
+        }
+    }
+
+
+    override fun dispose() {
+        testcaseListModel.removeListDataListener(this)
+    }
+
+
+    private fun updateTestcases(update: (MutableList<Testcase>) -> Unit) {
+        val testcases = db.solutionFiles[solutionFile.pathString]?.testcases?.toMutableList() ?: return
+        update(testcases)
+        db.updateSolutionFile(solutionFile.copy(testcases = testcases))
+    }
 }
+
 
 
