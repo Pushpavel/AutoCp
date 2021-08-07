ec05d9ba8dfea60460a16d691f2061a52022f46d
pushpavel <jpushpavel@gmail.com>
2021-08-08 00:11:51 +0530

    🐛: fix no quotes around paths in build command construction

--- src/main/kotlin/settings/langSettings/model/BuildConfig.kt
+++ src/main/kotlin/settings/langSettings/model/BuildConfig.kt
@@ -1,9 +1,9 @@
 package settings.langSettings.model
 
 import com.intellij.icons.AllIcons
+import common.ui.swing.TileCellRenderer
 import kotlinx.serialization.Serializable
 import settings.generalSettings.AutoCpGeneralSettings
-import common.ui.swing.TileCellRenderer
 
 @Serializable
 data class BuildConfig(
@@ -22,8 +22,8 @@ data class BuildConfig(
 
     fun constructBuildCommand(inputPath: String, outputPath: String): String {
         return buildCommand
-            .replace(AutoCpGeneralSettings.INPUT_PATH_KEY, inputPath)
-            .replace(AutoCpGeneralSettings.OUTPUT_PATH_KEY, outputPath)
+            .replace(AutoCpGeneralSettings.INPUT_PATH_KEY, "\"$inputPath\"")
+            .replace(AutoCpGeneralSettings.OUTPUT_PATH_KEY, "\"$outputPath\"")
     }
 
     constructor(m: MutableBuildConfig) : this(m.id, m.name, m.buildCommand)
