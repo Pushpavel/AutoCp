cf2a7be292ec1245d53cd885048d20d0e33412c6
pushpavel <jpushpavel@gmail.com>
2021-08-11 19:12:10 +0530

    🚧: added SimpleLocalServer.kt with error messages

--- src/main/kotlin/common/res/AutoCpStrings.kt
+++ src/main/kotlin/common/res/AutoCpStrings.kt
@@ -4,4 +4,20 @@ object AutoCpStrings {
     const val runConfigName = "AutoCp Solution"
     const val runConfigId = "AutoCp"
     const val runConfigDescription = "Test your Competitive Programming solution with AutoCp"
+
+
+    // Error messages
+    const val problemGatheringTitle = "Problem Gathering"
+
+    const val fileIssue = "please file an issue (https://github.com/Pushpavel/AutoCp/issues/new/choose)"
+
+    fun portTakenMsg(port: Int) = "Port $port is already in use."
+
+    fun portRetryMsg(port: Int) = "Retrying with port $port..."
+
+    fun allPortFailedMsg() = "Could not find a free port to use with competitive companion. " +
+            "You could be running multiple instance of AutoCp installed IDEs or any other tools that uses competitive companion. " +
+            "Close other programs or restart your pc. If this issue still occurs, " + fileIssue
 }
+
+fun String.failed(): String = "$this Failed"
---
+++ src/main/kotlin/gather/server/SimpleLocalServer.kt
@@ -0,0 +1,94 @@
+package gather.server
+
+import common.helpers.ioScope
+import common.helpers.notifyErr
+import common.helpers.notifyWarn
+import common.res.R
+import common.res.failed
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.InetAddress
+import java.net.ServerSocket
+import java.net.SocketException
+import java.net.SocketTimeoutException
+
+abstract class SimpleLocalServer(private val ports: List<Int>) {
+    private var serverSocket: ServerSocket? = null
+    private val scope = ioScope()
+
+    fun start() {
+        if (isActive()) return
+
+        var portIndex = 0
+
+        while (portIndex < ports.size) {
+
+            if (portIndex != 0)
+                notifyWarn(
+                    R.strings.problemGatheringTitle.failed(),
+                    R.strings.portTakenMsg(ports[portIndex - 1]) + " " + R.strings.portRetryMsg(ports[portIndex])
+                )
+
+            try {
+                serverSocket = ServerSocket(45, 50, InetAddress.getByName("localhost"))
+                onServerStart(serverSocket!!)
+                // successfully started the server
+                return
+            } catch (e: SocketException) {
+                // failed
+                // retrying with next port
+                portIndex++
+            }
+        }
+
+        if (portIndex != 0)
+            notifyErr(
+                R.strings.problemGatheringTitle.failed(),
+                R.strings.portTakenMsg(ports[portIndex - 1]) + " " + R.strings.allPortFailedMsg()
+            )
+    }
+
+    private fun onServerStart(serverSocket: ServerSocket) {
+        serverSocket.soTimeout = 1000
+        scope.launch(Dispatchers.IO) {
+            while (true) {
+                try {
+                    serverSocket.accept().use {
+                        val inputStream = it.getInputStream()
+                        val request = readFromStream(inputStream)
+                        val strings = request.split("\n\n".toPattern(), 2).toTypedArray()
+
+                        if (strings.size > 1)
+                            launch { onMessage(strings[1]) }
+
+                    }
+                } catch (e: SocketTimeoutException) {
+                    launch { onTimeout() }
+                } catch (e: SocketException) {
+                    // socket maybe closed, catch and ignore it
+                }
+            }
+        }
+    }
+
+    abstract suspend fun onMessage(message: String)
+
+    abstract suspend fun onTimeout()
+
+
+    private fun isActive(): Boolean {
+        return serverSocket != null && !serverSocket!!.isClosed
+    }
+
+    private fun readFromStream(inputStream: InputStream): String {
+        BufferedReader(InputStreamReader(inputStream, "UTF-8")).use { reader ->
+            val builder = StringBuilder()
+            var line: String?
+            while (reader.readLine().also { line = it } != null) builder.append(line).append('\n')
+            return builder.toString()
+        }
+    }
+}
