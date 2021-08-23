2e33559dd554d7f62382cf6a1dd60e34a3f894e8
pushpavel <jpushpavel@gmail.com>
2021-08-23 18:51:26 +0530

    🚧: separated default languages getting logic into getDefaultLanguages.kt

--- src/main/kotlin/common/res/AutoCpKeys.kt
+++ src/main/kotlin/common/res/AutoCpKeys.kt
@@ -3,5 +3,11 @@ package common.res
 object AutoCpKeys {
     const val pluginId = "com.github.pushpavel.autocp"
     const val toolWindowSelectedTabIndexKey = "$pluginId.toolWindowSelectedTabIndex"
+
+    // Command User Macros
     const val inputPathMacro = "@in"
+
+    // Command Plugin Macros
+    const val execExtensionMacro = "@exe"
+
 }
---
+++ src/main/kotlin/settings/langSettings/getDefaultLanguages.kt
@@ -0,0 +1,48 @@
+package settings.langSettings
+
+import com.intellij.execution.Platform
+import com.intellij.lang.Language
+import com.intellij.util.io.readText
+import common.res.R
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import settings.langSettings.model.Lang
+import java.nio.file.Path
+
+fun getDefaultLanguages(): Map<String, Lang> {
+    return R.files.langJsons
+        .map<Path, Lang> { Json.decodeFromString(it.readText()) }
+        // filtering languages supported by the IDE
+        .filter { Language.findLanguageByID(it.langId) != null }
+        .map { lang ->
+            val buildConfigs = lang.buildConfigs.mapValues {
+                it.value.copy(
+                    buildCommand = applyCommandMacros(it.value.buildCommand),
+                    executeCommand = applyCommandMacros(it.value.executeCommand)
+                )
+            }
+
+            lang.copy(buildConfigs = buildConfigs)
+        }
+        // converting to map
+        .associateBy { it.langId }
+}
+
+private fun applyCommandMacros(command: String): String {
+    var c = command
+    for (k in getPlatformMacros())
+        c = c.replace(k.key, k.value)
+    return c
+}
+
+
+private fun getPlatformMacros(): Map<String, String> {
+    return when (Platform.current()) {
+        Platform.WINDOWS -> mapOf(
+            R.keys.execExtensionMacro to ".exe"
+        )
+        Platform.UNIX -> mapOf(
+            R.keys.execExtensionMacro to ""
+        )
+    }
+}
