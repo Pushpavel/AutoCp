cfc128150900e5d9547fcd53d8e8712ecfcec405
pushpavel <jpushpavel@gmail.com>
2021-08-23 16:43:23 +0530

    🚧: updated BuildConfigDialog.kt UI with execute command and its validation

--- src/main/kotlin/common/res/AutoCpKeys.kt
+++ src/main/kotlin/common/res/AutoCpKeys.kt
@@ -3,4 +3,5 @@ package common.res
 object AutoCpKeys {
     const val pluginId = "com.github.pushpavel.autocp"
     const val toolWindowSelectedTabIndexKey = "$pluginId.toolWindowSelectedTabIndex"
+    const val inputPathMacro = "@in"
 }
--- src/main/kotlin/settings/langSettings/ui/dialogs/BuildConfigDialog.kt
+++ src/main/kotlin/settings/langSettings/ui/dialogs/BuildConfigDialog.kt
@@ -1,15 +1,13 @@
 package settings.langSettings.ui.dialogs
 
-import com.intellij.ide.macro.MacrosDialog
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.ValidationInfo
 import com.intellij.ui.layout.CCFlags
 import com.intellij.ui.layout.ValidationInfoBuilder
-import com.intellij.ui.layout.applyToComponent
 import com.intellij.ui.layout.panel
 import common.helpers.UniqueNameEnforcer
+import common.res.R
 import common.ui.dsl.withValidation
-import settings.generalSettings.AutoCpGeneralSettings
 import settings.langSettings.model.BuildConfig
 
 class BuildConfigDialog(
@@ -20,7 +18,7 @@ class BuildConfigDialog(
     var name = buildConfig.name
 
     @Suppress("MemberVisibilityCanBePrivate")
-    var commandTemplate = buildConfig.commandTemplate
+    var buildCommand = buildConfig.commandTemplate
     var executeCommand = buildConfig.executeCommand
 
     init {
@@ -34,23 +32,29 @@ class BuildConfigDialog(
 
     override fun createCenterPanel() = panel {
         row {
-            row("Name:") {
+            row("Name") {
                 textField(::name, 12)
                     .withValidation { validateName(it.text) }
             }
-            row("Command Template:") {
-                expandableTextField(::commandTemplate)
+
+            row(R.strings.buildCommandLabel) {
+                expandableTextField(::buildCommand)
                     .constraints(CCFlags.growX)
-                    .applyToComponent { MacrosDialog.addTextFieldExtension(this) }
-                    .withValidation { validateBuildCommand(it.text) }
-                    .comment(
-                        "AutoCp uses this template to construct a command that builds an executable " +
-                                "on which your testcases are tested, " +
-                                "@in in this template string would be replaced with path to solution file with quotes " +
-                                "and @out will be replaced with output path for the executable." +
-                                "@out can be ignored if executing this command does not produce any executable."
-                    )
+                    .comment(R.strings.buildCommandComment).component
             }
+
+            row(R.strings.executeCommandLabel) {
+                expandableTextField(::executeCommand)
+                    .constraints(CCFlags.growX)
+                    .comment(R.strings.executeCommandComment)
+                    .withValidationOnInput {
+                        if (it.text.isBlank())
+                            error("Must not be empty")
+                        else
+                            null
+                    }
+            }
+            commentRow(R.strings.commandTemplateDesc)
         }
     }.also { it.reset() }
 
@@ -58,7 +62,7 @@ class BuildConfigDialog(
         val confirm = showAndGet()
 
         return if (confirm)
-            BuildConfig(buildConfig.id, name, commandTemplate, executeCommand)
+            BuildConfig(buildConfig.id, name, buildCommand, executeCommand)
         else
             null
     }
@@ -72,16 +76,5 @@ class BuildConfigDialog(
             return error("\"$name\" already exists")
 
         return null
-    }
-
-    private fun ValidationInfoBuilder.validateBuildCommand(commandTemplate: String): ValidationInfo? {
-        val input = commandTemplate.contains(AutoCpGeneralSettings.INPUT_PATH_KEY)
-
-        val errorMessage = when {
-            !input -> "${AutoCpGeneralSettings.INPUT_PATH_KEY} missing, This will be replaced with path to solution file ex- \"C:\\solution.cpp\""
-            else -> null
-        }
-
-        return errorMessage?.let { error(it) }
     }
 }
