d3368be37e61ed2a435e633dbd4afd3418d89330
pushpavel <jpushpavel@gmail.com>
2021-08-26 01:41:21 +0530

    🐛: fix modality issues in CMakeAddExecutable.kt

--- src/main/kotlin/lang/cmake/CMakeAddExecutable.kt
+++ src/main/kotlin/lang/cmake/CMakeAddExecutable.kt
@@ -1,5 +1,8 @@
 package lang.cmake
 
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.invokeLater
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.application.runUndoTransparentWriteAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
@@ -19,28 +22,35 @@ class CMakeAddExecutable(val project: Project) : FileGenerationListener {
     val settings = project.cmakeSettings()
 
     override fun onGenerated(file: VirtualFile) {
+
         if (!settings.addToCMakeLists) return
 
-        val cmakeFile = LocalFileSystem.getInstance().findFileByIoFile(cmakePath) ?: return
-        val cmakeDoc = FileDocumentManager.getInstance().getDocument(cmakeFile) ?: return
-        val cmakeText = cmakeDoc.text
-        val relativePath = file.pathRelativeToProject()
-        val name = file.nameWithoutExtension
+        invokeLater(ModalityState.NON_MODAL) {
 
-        val regex = "add_executable\\s*\\(\\s*(\\S+)(?:\\s+(?:\"([^\"]+)|([^ \"]+))\\s*)?".toRegex()
-        val matches = regex.findAll(cmakeText)
+            val cmakeFile = LocalFileSystem.getInstance().findFileByIoFile(cmakePath)
+                ?: return@invokeLater
+            val cmakeDoc = runReadAction { FileDocumentManager.getInstance().getDocument(cmakeFile) }
+                ?: return@invokeLater
 
-        matches.forEachIndexed { index, match ->
-            if (match.groupValues[1] == name) {
-                runUndoTransparentWriteAction {
-                    val endIndex = matches.elementAt(index).groups[1]!!.range.last + 1
-                    cmakeDoc.insertString(endIndex, " \"${relativePath}\"")
+            val cmakeText = cmakeDoc.text
+            val relativePath = file.pathRelativeToProject()
+            val name = file.nameWithoutExtension
+
+            val regex = "add_executable\\s*\\(\\s*(\\S+)(?:\\s+(?:\"([^\"]+)|([^ \"]+))\\s*)?".toRegex()
+            val matches = regex.findAll(cmakeText)
+
+            matches.forEachIndexed { index, match ->
+                if (match.groupValues[1] == name) {
+                    runUndoTransparentWriteAction {
+                        val endIndex = matches.elementAt(index).groups[1]!!.range.last + 1
+                        cmakeDoc.insertString(endIndex, " \"${relativePath}\"")
+                    }
+                    return@invokeLater
                 }
-                return
             }
-        }
-        runUndoTransparentWriteAction {
-            cmakeDoc.insertString(cmakeText.length, "\nadd_executable($name \"${relativePath}\")")
+            runUndoTransparentWriteAction {
+                cmakeDoc.insertString(cmakeText.length, "\nadd_executable($name \"${relativePath}\")")
+            }
         }
     }
 
