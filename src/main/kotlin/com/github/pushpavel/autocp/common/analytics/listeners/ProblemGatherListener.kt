05023b735a66dd77bad97e8a728230d8b617dc07
Pushpavel <jpushpavel@gmail.com>
2021-10-07 13:34:13 +0530

    🚧: added BatchGatherEvent.kt

--- src/main/kotlin/com/github/pushpavel/autocp/common/analytics/listeners/ProblemGatherListener.kt
+++ src/main/kotlin/com/github/pushpavel/autocp/common/analytics/listeners/ProblemGatherListener.kt
@@ -1,13 +1,18 @@
 package com.github.pushpavel.autocp.common.analytics.listeners
 
+import com.github.pushpavel.autocp.common.analytics.GoogleAnalytics
+import com.github.pushpavel.autocp.common.analytics.events.BatchGatherEvent
 import com.github.pushpavel.autocp.database.models.Problem
 import com.github.pushpavel.autocp.gather.filegen.FileGenerationListener
 import com.github.pushpavel.autocp.gather.models.BatchJson
 import com.intellij.openapi.vfs.VirtualFile
 
 class ProblemGatherListener : FileGenerationListener {
+    var currentBatch: BatchJson? = null
     override fun onGenerated(file: VirtualFile, problem: Problem, batch: BatchJson, extension: String) {
-//        if (!alreadyGenerated)
-//            GoogleAnalytics.instance.sendEvent(ProblemGatheredEvent(problem, lang))
+        if (currentBatch != batch) {
+            GoogleAnalytics.instance.sendEvent(BatchGatherEvent(problem.groupName, extension, batch.size))
+            currentBatch = batch
+        }
     }
 }
