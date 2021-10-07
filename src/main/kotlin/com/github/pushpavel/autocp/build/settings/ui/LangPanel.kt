946b0ab6c121fd1c7200317ca75fb2e487ca94df
Pushpavel <jpushpavel@gmail.com>
2021-10-07 14:43:42 +0530

    🚧: trimmed commands in LangSettings

--- src/main/kotlin/com/github/pushpavel/autocp/build/settings/ui/LangPanel.kt
+++ src/main/kotlin/com/github/pushpavel/autocp/build/settings/ui/LangPanel.kt
@@ -7,6 +7,7 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.runUndoTransparentWriteAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorFactory
+import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.ui.CollectionListModel
 import com.intellij.ui.layout.CCFlags
@@ -20,8 +21,10 @@ class LangPanel(val model: CollectionListModel<Lang>) : DslCallbacks, Disposable
     private val buildCommandDoc = editorFactory.createDocument("")
     private val executeCommandDoc = editorFactory.createDocument("")
 
-    private val buildCommandEditor = editorFactory.createEditor(buildCommandDoc).apply { customizeEditor() }
-    private val executeCommandEditor = editorFactory.createEditor(executeCommandDoc).apply { customizeEditor() }
+    private val buildCommandEditor =
+        editorFactory.createEditor(buildCommandDoc).apply { customizeEditor() }
+    private val executeCommandEditor =
+        editorFactory.createEditor(executeCommandDoc).apply { customizeEditor() }
     var lineCommentPrefix = ""
 
     val component: DialogPanel = panel {
@@ -46,10 +49,15 @@ class LangPanel(val model: CollectionListModel<Lang>) : DslCallbacks, Disposable
 
 
     private fun Editor.customizeEditor() {
+        if (this is EditorEx)
+            isOneLineMode = true
+
         settings.apply {
             isUseSoftWraps = true
             additionalLinesCount = 0
             isLineMarkerAreaShown = false
+            isLineNumbersShown = false
+            setUseTabCharacter(false)
         }
     }
 
@@ -68,17 +76,17 @@ class LangPanel(val model: CollectionListModel<Lang>) : DslCallbacks, Disposable
 
     override fun isModified(): Boolean {
         return lang != null
-                && (lang!!.buildCommand != buildCommandDoc.text.takeIf { it.isNotBlank() }
-                || lang!!.executeCommand != executeCommandDoc.text
-                || lang!!.lineCommentPrefix != lineCommentPrefix
+                && (lang!!.buildCommand != buildCommandDoc.text.takeIf { it.isNotBlank() }?.trim()
+                || lang!!.executeCommand != executeCommandDoc.text.trim()
+                || lang!!.lineCommentPrefix != lineCommentPrefix.trim()
                 )
     }
 
     override fun apply() {
         lang = lang?.copy(
-            buildCommand = buildCommandDoc.text.takeIf { it.isNotBlank() },
-            executeCommand = executeCommandDoc.text,
-            lineCommentPrefix = lineCommentPrefix
+            buildCommand = buildCommandDoc.text.takeIf { it.isNotBlank() }?.trim(),
+            executeCommand = executeCommandDoc.text.trim(),
+            lineCommentPrefix = lineCommentPrefix.trim()
         )
     }
 
