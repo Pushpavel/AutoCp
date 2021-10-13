8a143b02553cb76197943db235cf94123fb1b808
Pushpavel <jpushpavel@gmail.com>
2021-10-13 21:50:29 +0530

    🚧: added searchbar

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,20 +1,21 @@
-import { defineUserConfig } from "vuepress";
-import type { DefaultThemeOptions } from "vuepress";
+import {defineUserConfig} from "vuepress";
+import type {DefaultThemeOptions} from "vuepress";
 
 export default defineUserConfig<DefaultThemeOptions>({
-  lang: "en-US",
-  base: "/AutoCp/",
-  bundler: "@vuepress/bundler-vite",
-  head: [
-    ["link", { rel: "icon", href: "/AutoCp/assets/logo.svg?" }],
-    ["script",{ src: "//plugins.jetbrains.com/assets/scripts/mp-widget.js" }],
-    ["script",{},`MarketplaceWidget.setupMarketplaceWidget('install', 17061, "#installBtn");`]
-  ],
+    lang: "en-US",
+    base: "/AutoCp/",
+    bundler: "@vuepress/bundler-vite",
+    head: [
+        ["link", {rel: "icon", href: "/AutoCp/assets/logo.svg?"}],
+        ["script", {src: "//plugins.jetbrains.com/assets/scripts/mp-widget.js"}],
+        ["script", {}, `MarketplaceWidget.setupMarketplaceWidget('install', 17061, "#installBtn");`]
+    ],
 
-  title: "AutoCp",
-  description:
-    "An Open Source Competitive Programming Plugin for Intellij-Based IDEs",
-  themeConfig: {
-    logo: "/assets/logo.svg",
-  },
+    title: "AutoCp",
+    description:
+        "An Open Source Competitive Programming Plugin for Intellij-Based IDEs",
+    themeConfig: {
+        logo: "/assets/logo.svg",
+    },
+    plugins: ['@vuepress/plugin-search']
 });
