988cfb222dd0f5480ece3fc07ca25a1c4bb2aaae
Pushpavel <jpushpavel@gmail.com>
2021-10-13 23:27:22 +0530

    ♻️: moved html head config to an explicit template.html file

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,21 +1,15 @@
 import {defineUserConfig} from "vuepress";
 import type {DefaultThemeOptions} from "vuepress";
 
+const path = require("path");
+
 export default defineUserConfig<DefaultThemeOptions>({
     lang: "en-US",
     base: "/AutoCp/",
     bundler: "@vuepress/bundler-vite",
-    head: [
-        ["link", {rel: "icon", href: "/AutoCp/assets/logo.svg?"}],
-        ["script", {src: "//plugins.jetbrains.com/assets/scripts/mp-widget.js"}],
-        ["script", {}, `MarketplaceWidget.setupMarketplaceWidget('install', 17061, "#installBtn");`]
-    ],
-
     title: "AutoCp",
-    description:
-        "An Open Source Competitive Programming Plugin for Intellij-Based IDEs",
-    themeConfig: {
-        logo: "/assets/logo.svg",
-    },
+    description: "An Open Source Competitive Programming Plugin for Intellij-Based IDEs",
+    themeConfig: {logo: "/assets/logo.svg"},
+    templateDev: path.resolve(__dirname, "template.html"),
     plugins: ['@vuepress/plugin-search']
 });
