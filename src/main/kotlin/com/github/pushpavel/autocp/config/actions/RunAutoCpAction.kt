0b264fe601f347356f0c61ba903403081dd0b7b1
Pushpavel <jpushpavel@gmail.com>
2021-10-20 21:50:06 +0530

    💚: fix plugin verifier compatibility issue by using deprecated method

--- src/main/kotlin/com/github/pushpavel/autocp/config/actions/RunAutoCpAction.kt
+++ src/main/kotlin/com/github/pushpavel/autocp/config/actions/RunAutoCpAction.kt
@@ -16,11 +16,7 @@ class RunAutoCpAction : AnAction("Run with AutoCp", "Run the currently focused f
     override fun actionPerformed(e: AnActionEvent) {
         try {
             val configProducer = RunConfigurationProducer.getInstance(AutoCpConfigProducer::class.java)
-            val configurationContext = try {
-                ConfigurationContext.getFromContext(e.dataContext, e.place)
-            } catch (error: NoSuchMethodError) {
-                ConfigurationContext.getFromContext(e.dataContext)
-            }
+            val configurationContext = ConfigurationContext.getFromContext(e.dataContext)
             val configurationFromContext = configProducer.createConfigurationFromContext(configurationContext)
 
             val runnerAndConfigurationSettings =
