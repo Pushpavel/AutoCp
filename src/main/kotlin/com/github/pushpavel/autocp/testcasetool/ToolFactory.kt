efc57baf576c4dae81281f625baea6bd0bac92b7
CS19B1042 Pushpavel <jpushpavel@gmail.com>
2021-11-04 22:18:05 +0530

    ♻️: change rightAnchored to bottomAnchored boolean in TestcaseToolBarsPanel.kt

--- src/main/kotlin/com/github/pushpavel/autocp/testcasetool/ToolFactory.kt
+++ src/main/kotlin/com/github/pushpavel/autocp/testcasetool/ToolFactory.kt
@@ -1,38 +1,15 @@
 package com.github.pushpavel.autocp.testcasetool
 
-import com.github.pushpavel.autocp.testcasetool.components.TestcaseToolBars
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
-import com.intellij.openapi.wm.ToolWindowAnchor
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.ui.AncestorListenerAdapter
-import com.intellij.ui.layout.panel
-import javax.swing.event.AncestorEvent
 
 class ToolFactory : ToolWindowFactory, DumbAware {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val contentManager = toolWindow.contentManager
-        val component = toolWindowContentUI(toolWindow)
-        val content = contentManager.factory.createContent(component, null, false)
-        contentManager.addContent(content)
-    }
-
-    private fun toolWindowContentUI(toolWindow: ToolWindow): TestcaseToolBars {
-        val root = TestcaseToolBars(
-            toolWindow.anchor != ToolWindowAnchor.BOTTOM,
-            panel { row { label("content") } }
-        )
-
-        // listen for ancestor changes for reacting to toolWindow anchor changes
-        root.addAncestorListener(object : AncestorListenerAdapter() {
-            override fun ancestorAdded(event: AncestorEvent?) {
-                root.rightAnchored = toolWindow.anchor != ToolWindowAnchor.BOTTOM
-                root.applyUpdates()
-                root.invalidate()
-            }
-        })
-
-        return root
+//        val component = TestcaseToolContentPanel(toolWindow)
+//        val content = contentManager.factory.createContent(component, null, false)
+//        contentManager.addContent(content)
     }
 }
--- src/main/kotlin/com/github/pushpavel/autocp/testcasetool/components/TestcaseToolBarsPanel.kt
+++ src/main/kotlin/com/github/pushpavel/autocp/testcasetool/components/TestcaseToolBarsPanel.kt
@@ -9,35 +9,37 @@ import java.awt.BorderLayout
 import javax.swing.JComponent
 import javax.swing.SwingConstants
 
-class TestcaseToolBars(var rightAnchored: Boolean = true, val content: JComponent) :
-    JBPanel<TestcaseToolBars>(BorderLayout()) {
+abstract class TestcaseToolBarsPanel : JBPanel<TestcaseToolBarsPanel>(BorderLayout()) {
+
+    var bottomAnchored: Boolean = true
+    abstract val content: JComponent
 
     private val runActionBar = ActionManager.getInstance().createActionToolbar(
         ActionPlaces.TOOLWINDOW_TOOLBAR_BAR,
         actionGroup(),
-        rightAnchored
+        !bottomAnchored
     )
 
     private val testcaseActionBar = ActionManager.getInstance().createActionToolbar(
         ActionPlaces.TOOLWINDOW_TOOLBAR_BAR,
         testcaseActionGroup(),
-        !rightAnchored
+        bottomAnchored
     )
 
     private val innerPanel = BorderLayoutPanel()
 
-    fun applyUpdates() {
+    open fun applyUpdates() {
         runActionBar.targetComponent = content
         testcaseActionBar.targetComponent = content
 
-        runActionBar.setOrientation(if (rightAnchored) SwingConstants.HORIZONTAL else SwingConstants.VERTICAL)
+        runActionBar.setOrientation(if (!bottomAnchored) SwingConstants.HORIZONTAL else SwingConstants.VERTICAL)
         remove(runActionBar.component)
-        add(runActionBar.component, if (rightAnchored) BorderLayout.PAGE_START else BorderLayout.LINE_START)
+        add(runActionBar.component, if (!bottomAnchored) BorderLayout.PAGE_START else BorderLayout.LINE_START)
 
         add(innerPanel.apply {
-            testcaseActionBar.setOrientation(if (rightAnchored) SwingConstants.VERTICAL else SwingConstants.HORIZONTAL)
+            testcaseActionBar.setOrientation(if (!bottomAnchored) SwingConstants.VERTICAL else SwingConstants.HORIZONTAL)
             remove(testcaseActionBar.component)
-            add(testcaseActionBar.component, if (rightAnchored) BorderLayout.LINE_START else BorderLayout.PAGE_START)
+            add(testcaseActionBar.component, if (!bottomAnchored) BorderLayout.LINE_START else BorderLayout.PAGE_START)
 
             remove(content)
             add(content, BorderLayout.CENTER)
