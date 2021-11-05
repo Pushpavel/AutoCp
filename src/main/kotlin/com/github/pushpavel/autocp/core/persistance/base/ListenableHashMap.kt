e566d11f576711fc518e4e7269de5c78cdc74f92
CS19B1042 Pushpavel <jpushpavel@gmail.com>
2021-11-05 08:27:45 +0530

    ♻️: event enums to sealed interface in ListenableHashMap.kt

--- src/main/kotlin/com/github/pushpavel/autocp/core/persistance/base/ListenableHashMap.kt
+++ src/main/kotlin/com/github/pushpavel/autocp/core/persistance/base/ListenableHashMap.kt
@@ -3,43 +3,51 @@ package com.github.pushpavel.autocp.core.persistance.base
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.util.Disposer
 
-typealias Handler<V> = ListenableHashMap.Event.(V) -> Unit
+typealias Handler<K, V> = (ListenableHashMap.Event<K, V>) -> Unit
 
 open class ListenableHashMap<K, V> : ProtectedHashMap<K, V>() {
 
-    enum class Event { PUT, REMOVE, REPLACE }
+    sealed interface Event<K, V> {
+        val key: K
+        val value: V?
 
-    private val putEventListeners = mutableSetOf<Handler<V>>()
-    private val removeEventListeners = mutableSetOf<Handler<V>>()
-    private val replaceEventListeners = mutableSetOf<Handler<V>>()
-    private val putListeners = mutableMapOf<K, MutableList<Handler<V>>>()
-    private val removeListeners = mutableMapOf<K, MutableList<Handler<V>>>()
-    private val replaceListeners = mutableMapOf<K, MutableList<Handler<V>>>()
+        data class Put<K, V>(override val key: K, override val value: V) : Event<K, V>
+        data class Remove<K, V>(override val key: K, override val value: V) : Event<K, V>
+        data class Replace<K, V>(override val key: K, override val value: V, val oldValue: V) : Event<K, V>
+        data class Replay<K, V>(override val key: K, override val value: V?) : Event<K, V>
+    }
+
+    private val putEventListeners = mutableSetOf<Handler<K, V>>()
+    private val removeEventListeners = mutableSetOf<Handler<K, V>>()
+    private val replaceEventListeners = mutableSetOf<Handler<K, V>>()
+    private val putListeners = mutableMapOf<K, MutableList<Handler<K, V>>>()
+    private val removeListeners = mutableMapOf<K, MutableList<Handler<K, V>>>()
+    private val replaceListeners = mutableMapOf<K, MutableList<Handler<K, V>>>()
 
     override fun put(key: K, value: V) {
         super.put(key, value)
-        putEventListeners.forEach { Event.PUT.it(value) }
-        putListeners[key]?.onEach { Event.PUT.it(value) }
+        putEventListeners.forEach { it(Event.Put(key, value)) }
+        putListeners[key]?.onEach { it(Event.Put(key, value)) }
     }
 
     override fun remove(key: K): V {
         val oldValue = super.remove(key)
-        removeEventListeners.forEach { Event.REMOVE.it(oldValue) }
-        removeListeners[key]?.onEach { Event.REMOVE.it(oldValue) }
+        removeEventListeners.forEach { it(Event.Remove(key, oldValue)) }
+        removeListeners[key]?.onEach { it(Event.Remove(key, oldValue)) }
         return oldValue
     }
 
     override fun replace(key: K, value: V): V {
         val oldValue = super.replace(key, value)
-        replaceEventListeners.forEach { Event.REPLACE.it(oldValue) }
-        replaceListeners[key]?.onEach { Event.REPLACE.it(oldValue) }
+        replaceEventListeners.forEach { it(Event.Replace(key, value, oldValue)) }
+        replaceListeners[key]?.onEach { it(Event.Replace(key, value, oldValue)) }
         return oldValue
     }
 
     /**
      * Adds a listener that will be called when a new value is put into the map.
      */
-    fun onPut(listener: Handler<V>, parentDisposable: Disposable) {
+    fun onPut(listener: Handler<K, V>, parentDisposable: Disposable) {
         putEventListeners.add(listener)
         Disposer.register(parentDisposable) { putEventListeners.remove(listener) }
     }
@@ -47,7 +55,7 @@ open class ListenableHashMap<K, V> : ProtectedHashMap<K, V>() {
     /**
      * Adds a listener that will be called when a value is removed from the map.
      */
-    fun onRemove(listener: Handler<V>, parentDisposable: Disposable) {
+    fun onRemove(listener: Handler<K, V>, parentDisposable: Disposable) {
         removeEventListeners.add(listener)
         Disposer.register(parentDisposable) { removeEventListeners.remove(listener) }
     }
@@ -55,7 +63,7 @@ open class ListenableHashMap<K, V> : ProtectedHashMap<K, V>() {
     /**
      * Adds a listener that will be called when a value is replaced in the map.
      */
-    fun onReplace(listener: Handler<V>, parentDisposable: Disposable) {
+    fun onReplace(listener: Handler<K, V>, parentDisposable: Disposable) {
         replaceEventListeners.add(listener)
         Disposer.register(parentDisposable) { replaceEventListeners.remove(listener) }
     }
@@ -65,12 +73,16 @@ open class ListenableHashMap<K, V> : ProtectedHashMap<K, V>() {
      */
     fun onKey(
         key: K,
-        listener: Handler<V>,
+        listener: Handler<K, V>,
         parentDisposable: Disposable,
         replace: Boolean = true,
         put: Boolean = true,
-        remove: Boolean = true
+        remove: Boolean = true,
+        replay: Boolean = false,
     ) {
+        if (replay)
+            listener(Event.Replay(key, get(key)))
+
         if (put) putListeners.getOrPut(key) { mutableListOf() }.add(listener)
         if (replace) replaceListeners.getOrPut(key) { mutableListOf() }.add(listener)
         if (remove) removeListeners.getOrPut(key) { mutableListOf() }.add(listener)
