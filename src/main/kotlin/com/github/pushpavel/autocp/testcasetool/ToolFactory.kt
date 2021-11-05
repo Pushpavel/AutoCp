87e36e583fd24635f4da4ada36aa47af55bb36ed
CS19B1042 Pushpavel <jpushpavel@gmail.com>
2021-11-05 20:41:56 +0530

    🚧: implement toolWindow content management

--- src/main/kotlin/com/github/pushpavel/autocp/testcasetool/ToolFactory.kt
+++ src/main/kotlin/com/github/pushpavel/autocp/testcasetool/ToolFactory.kt
@@ -1,5 +1,7 @@
 package com.github.pushpavel.autocp.testcasetool
 
+import com.github.pushpavel.autocp.common.helpers.onFileSelectionChange
+import com.github.pushpavel.autocp.testcasetool.datalayer.TestcaseSolutionContentManager
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
@@ -7,9 +9,7 @@ import com.intellij.openapi.wm.ToolWindowFactory
 
 class ToolFactory : ToolWindowFactory, DumbAware {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val contentManager = toolWindow.contentManager
-//        val component = TestcaseToolContentPanel(toolWindow)
-//        val content = contentManager.factory.createContent(component, null, false)
-//        contentManager.addContent(content)
+        val manager = TestcaseSolutionContentManager(project, toolWindow)
+        project.onFileSelectionChange { manager.changeSolutionFile(it) }
     }
 }
---
+++ src/main/kotlin/com/github/pushpavel/autocp/testcasetool/datalayer/TestcaseSolutionContentManager.kt
@@ -0,0 +1,76 @@
+package com.github.pushpavel.autocp.testcasetool.datalayer
+
+import com.github.pushpavel.autocp.build.settings.LangSettings
+import com.github.pushpavel.autocp.common.helpers.*
+import com.github.pushpavel.autocp.core.persistance.solutions.Solutions
+import com.github.pushpavel.autocp.testcasetool.components.TestcaseToolContentPanel
+import com.intellij.openapi.components.service
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.wm.ToolWindow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.swing.JComponent
+
+class TestcaseSolutionContentManager(private val project: Project, private val toolWindow: ToolWindow) {
+    private val editorManager = FileEditorManager.getInstance(project)
+    private val contentManager = toolWindow.contentManager
+
+    private val solutions = project.service<Solutions>()
+    private val fileScoped by DisposableScope()
+    private val contentScoped by DisposableScope()
+
+    /**
+     * Rebuilds the toolWindow content with currently selected [file]
+     */
+    fun changeSolutionFile(file: VirtualFile?): Boolean {
+        fileScoped.doDisposal() // clear previous file related resources
+
+        // show no content if no valid file
+        if (file == null || !file.isValid || !editorManager.isFileOpen(file))
+            return false
+
+        val solutionKey = file.pathString
+        val component = createComponent(solutionKey, file.extension)
+
+        if (component != null) {
+            val content = contentManager.factory.createContent(component, null /*TODO*/, false)
+            contentManager.addContent(content)
+            // remove content scope on file change
+            Disposer.register(fileScoped, contentScoped)
+            Disposer.register(contentScoped) { contentManager.removeContent(content, true) }
+            return true
+        }
+        return false
+    }
+
+    /**
+     * Creates a component for the given [solutionKey] and [extension]
+     * Provides UI to add [solutionKey] if not already added to [solutions]
+     */
+    private fun createComponent(solutionKey: String, extension: String?): JComponent? {
+        if (solutionKey !in solutions) {
+            if (extension !in LangSettings.instance.langs)
+                return null
+            // TODO: option to associate currently selected file as Solution
+            return null
+        }
+
+        val scope = defaultScope().cancelBy(fileScoped)
+
+        val contentPanel = TestcaseToolContentPanel(project, toolWindow, solutions[solutionKey]!!)
+
+        // listen for changes in the solution
+        scope.launch {
+            solutions.onKey(solutionKey).collect {
+                if (it != null)
+                    contentPanel.solution = it
+                else
+                    contentScoped.doDisposal()
+            }
+        }
+        return contentPanel.testcaseListPanel
+    }
+}
