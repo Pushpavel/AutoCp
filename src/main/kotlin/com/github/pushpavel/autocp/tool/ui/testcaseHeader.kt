33314dd5d76859ebe4b1bcb00b8136cf616d6bb7
Pushpavel <jpushpavel@gmail.com>
2021-12-07 19:20:50 +0530

    👽: refactor "ActionToolbar.targetComponent" due to some IDE breaking change

--- src/main/kotlin/com/github/pushpavel/autocp/tool/ui/testcaseHeader.kt
+++ src/main/kotlin/com/github/pushpavel/autocp/tool/ui/testcaseHeader.kt
@@ -42,7 +42,7 @@ fun testcaseHeader(
             .getInstance()
             .createActionToolbar(ActionPlaces.TOOLWINDOW_CONTENT, actionGroup, true).apply {
                 layoutPolicy = ActionToolbar.NOWRAP_LAYOUT_POLICY
-            }.apply { targetComponent = jbPanel }
+            }.apply { setTargetComponent(jbPanel) }
             .component,
         BorderLayout.LINE_END
     )
