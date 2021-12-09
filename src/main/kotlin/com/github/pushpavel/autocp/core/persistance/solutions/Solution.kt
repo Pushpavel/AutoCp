a7937373b1bee627a777e337b51a2096db8734c4
Pushpavel <jpushpavel@gmail.com>
2021-12-09 18:44:44 +0530

    🚧: implement Storable interface on Solutions class

--- src/main/kotlin/com/github/pushpavel/autocp/core/persistance/solutions/Solution.kt
+++ src/main/kotlin/com/github/pushpavel/autocp/core/persistance/solutions/Solution.kt
@@ -1,7 +1,27 @@
 package com.github.pushpavel.autocp.core.persistance.solutions
 
+import com.google.gson.JsonObject
+
 data class Solution(
     val displayName: String,
     val pathString: String,
     val timeLimit: Int = 1000,
-)
+) {
+    fun toJson(): JsonObject {
+        val json = JsonObject()
+        json.addProperty("displayName", displayName)
+        json.addProperty("pathString", pathString)
+        json.addProperty("timeLimit", timeLimit)
+        return json
+    }
+
+    companion object {
+        fun fromJson(json: JsonObject): Solution {
+            return Solution(
+                displayName = json.get("displayName").asString,
+                pathString = json.get("pathString").asString,
+                timeLimit = json.get("timeLimit").asInt
+            )
+        }
+    }
+}
--- src/main/kotlin/com/github/pushpavel/autocp/core/persistance/solutions/Solutions.kt
+++ src/main/kotlin/com/github/pushpavel/autocp/core/persistance/solutions/Solutions.kt
@@ -1,6 +1,8 @@
 package com.github.pushpavel.autocp.core.persistance.solutions
 
 import com.github.pushpavel.autocp.core.persistance.base.MapWithEventFlow
+import com.github.pushpavel.autocp.core.persistance.storage.Storable
+import com.google.gson.JsonObject
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
 import kotlinx.coroutines.flow.filter
@@ -8,11 +10,21 @@ import kotlinx.coroutines.flow.map
 
 
 @Service
-class Solutions(project: Project) : MapWithEventFlow<String, Solution>() {
+class Solutions(project: Project) : MapWithEventFlow<String, Solution>(), Storable {
 
     fun onKey(key: String) = events.filter { it.keys.contains(key) }.map { it.map[key] }
 
     fun put(value: Solution) = put(value.pathString, value)
+    override val id = "solutions"
 
-    // TODO: import values from project
+    override fun load(data: JsonObject) {
+        clear()
+        data.entrySet().forEach { map[it.key] = Solution.fromJson(it.value.asJsonObject) }
+    }
+
+    override fun save(): JsonObject {
+        val json = JsonObject()
+        forEach { (key, value) -> json.add(key, value.toJson()) }
+        return json
+    }
 }
